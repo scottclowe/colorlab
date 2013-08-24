e674bfe8bd23a7d6176cb0f739ef2c7eaf407e37
Scott Lowe <scottlowe0@gmail.com>
2013-08-24 13:17:24 +0100

    CMYK and CIELab/UPLab conversion using ICC profiles

---
+++ cmyk2lab.m
@@ -0,0 +1,36 @@
+function Lab = cmyk2lab(cmyk, use_cielab, cmyk_space)
+
+if nargin<2
+    use_cielab = false;
+end
+if nargin<3
+    cmyk_space = 'eu'; % It is the ISO standard...
+end
+
+% http://www.ne14design.co.uk/articles/which-cmyk-profile.htm
+% http://www.color.org/chardata/drsection1.xalter
+% http://www.color.org/registry/index.xalter
+% http://www.eci.org/en/downloads
+% http://www.jpma-net.or.jp/en/
+switch lower(cmyk_space)
+    case 'eu'
+        icc_file = 'ISOcoated_v2_300_eci.icc'; % From ECI. ISO 12647/2-2004, v2
+    case 'us'
+%         icc_file = 'WebCoatedSWOP2006Grade5.icc'; % From Adobe - 2008
+        icc_file = 'SWOP2006_Coated5v2.icc'; % From IDEAlliance, via ICC - 2010
+    case 'japan'
+        icc_file = 'JapanColor2003WebCoated.icc'; % From Adobe. Might not be the best option.
+    otherwise
+        error('Unfamiliar CMYK space');
+end
+
+P_cmyk = iccread(icc_file);
+
+if use_cielab
+    P_uplab = iccread('CIELab_to_UPLab.icc');
+    Lab = applycform(cmyk, makecform('icc', P_cmyk, P_uplab));
+else
+    Lab = applycform(cmyk, makecform('CLUT', P_cmyk, 'AToB0')); % Pick one of BToA0, BToA1, BToA2
+end
+
+end
