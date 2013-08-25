21dcf91ad91f0da713ae94b5927db78b94035125
Scott Lowe <scottlowe0@gmail.com>
2013-08-25 17:50:55 +0100

    Improvements to parameter space display

--- development/find_pinchedspiral.m
+++ development/find_pinchedspiral.m
@@ -1,42 +1,63 @@
 
-use_uplab = false;
+% handpicked_hue1 = 13;
+% handpicked_hue2 = 87;
+% 
+% handpicked_hue1 = 21;
+% handpicked_hue2 = 75;
 
-g = fetch_cielchab_gamut('srgb', [], [], use_uplab);
 
-% Start values
-npoints = 100;
-expnt = 2;
-L_off = 0;
-L0 = 5;
-Lmax = 95;
-% c0 = 0;
-typ = 'pow';
-
-L = linspace(L0,Lmax,npoints);
-Lmid = (L0+Lmax)/2;
-
+% All up to one complete twist
 % hue1_range = 0:359;
 % hue2_range = -360:720;
 
-hue1_range =    0:2:359;
-hue2_range = -360:2:720;
+% % All up to one complete twist, reduced sampling
+% hue1_range =    0:2:359;
+% hue2_range = -360:2:720;
+% 
+% % Hot seach space
+% hue1_range = -45:1:45;
+% hue2_range =  70:1:130;
+
+% Hot seach space (tighter)
+hue1_range =  0:1:30;
+hue2_range = 70:1:110;
+
+
+
+% Curve parameters
+use_uplab = false;
+npoints = 100;  % number of points to use in test series
+expnt = 2.3;    % exponent to use for chroma curve
+L_off = 0;      % offset to exponent (not for sine curve)
+c0 = 0.3;       % proportion of maxc to add to all chroma (70<maxc<80)
+typ = 'pow';    % 'pow' or 'sin'
+Lmin = 5;       % minimum lightness (start)
+Lmax = 95;      % maximum lightness (end)
+
+% Computationally defined parameters
+L = linspace(Lmin,Lmax,npoints);
+Lmid = (Lmin+Lmax)/2;
+
+
+g = fetch_cielchab_gamut('srgb', [], [], use_uplab);
 
 all_maxc = nan(length(hue1_range),length(hue2_range));
+all_eucl = nan(length(hue1_range),length(hue2_range));
 
 for ih1=1:length(hue1_range)
     for ih2=1:length(hue2_range);
         h1 = hue1_range(ih1);
         h2 = hue2_range(ih2);
-        h_per_L = (h2-h1)/(Lmax-L0);
+        h_per_L = (h2-h1)/(Lmax-Lmin);
 
-        h = h1 + h_per_L * ((L-L0) + L_off - (L-Lmid).^2 * L_off/(Lmax-Lmid)^2);
+        h = h1 + h_per_L * ((L-Lmin) + L_off - (L-Lmid).^2 * L_off/(Lmax-Lmid)^2);
         h = mod(h,360);
         
         switch typ
             case 'sin'
-                c = sin(pi* (L-L0)/(Lmax-L0) ).^expnt;
+                c = c0 + (1-c0) * sin(pi* (L-Lmin)/(Lmax-Lmin) ).^expnt;
             case 'pow'
-                c = 1 - abs(((L-Lmid)*(min(Lmid-0,100-Lmid)/min(Lmax-Lmid,Lmid-L0))).^expnt) / abs(Lmid.^expnt);
+                c = 1 - (1-c0) * abs(((L-Lmid)*(min(Lmid-0,100-Lmid)/min(Lmax-Lmid,Lmid-Lmin))).^expnt) / abs(Lmid.^expnt);
             otherwise
                 error('Unfamiliar type');
         end
@@ -45,13 +66,25 @@ for ih1=1:length(hue1_range)
 
         % Check for points out of gamut
         [TF,P2] = isingamut(Lch,g,'Lch');
-
+        
         P2C = P2(:,2);
         maxc = min(P2C./c');
-        
         all_maxc(ih1,ih2) = maxc;
+        
+        % Euclidean length
+        c = c * maxc;
+        a = c.*cosd(h);
+        b = c.*sind(h);
+        Lab = [L' a' b'];
+        Lab_dif = diff(Lab,1,1);
+        Lab_sep = sqrt(sum(Lab_dif.^2,2));
+        Lab_len = sum(Lab_sep,1);
+        
+        all_eucl(ih1,ih2) = Lab_len;
     end
 end
+
+all_both = all_maxc/max(all_maxc(:))+all_eucl/max(all_eucl(:));
 
 %%
 
@@ -59,30 +92,79 @@ figure;
 imagesc(hue2_range, hue1_range, all_maxc);
 colormap(cie_hot);
 colorbar;
-ylabel(sprintf('Start hue at L=%.1f',L0));
+ylabel(sprintf('Start hue at L=%.1f',Lmin));
 xlabel(sprintf('End hue at L=%.1f',Lmax));
+title('Maximum chroma');
+
+figure;
+imagesc(hue2_range, hue1_range, all_eucl);
+colormap(cie_hot);
+colorbar;
+ylabel(sprintf('Start hue at L=%.1f',Lmin));
+xlabel(sprintf('End hue at L=%.1f',Lmax));
+title('Euclidean curve length');
+
+figure;
+imagesc(hue2_range,hue1_range,all_both);
+colormap(cie_hot);
+colorbar;
+ylabel(sprintf('Start hue at L=%.1f',Lmin));
+xlabel(sprintf('End hue at L=%.1f',Lmax));
+title('Sum');
 
 %%
-return;
+% return;
 
 %%
 
 % h1 = 275;
 % h2 = 360;
 
-h1 = 290;
-h2 = 380;
+% h1 = 290;
+% h2 = 380;
 
-h_per_L = (h2-h1)/(Lmax-L0);
 
-h = h1 + h_per_L * ((L-L0) + L_off - (L-Lmid).^2 * L_off/(Lmax-Lmid)^2);
+
+for i=1:3
+    switch i
+        case 0
+            if ~exist('handpicked_hue1','var') || ~exist('handpicked_hue2','var')
+                continue;
+            end
+            h1 = handpicked_hue1;
+            h2 = handpicked_hue2;
+            casediscr = 'Handpicked';
+        case 1
+            [mm,mI] = max(all_maxc(:));
+            [mih1,mih2] = ind2sub(size(all_maxc),mI);
+            h1 = hue1_range(mih1);
+            h2 = hue2_range(mih2);
+            casediscr = sprintf('Max Chroma = %.2f',mm);
+        case 2
+            [mm,mI] = max(all_eucl(:));
+            [mih1,mih2] = ind2sub(size(all_maxc),mI);
+            h1 = hue1_range(mih1);
+            h2 = hue2_range(mih2);
+            casediscr = sprintf('Max Euclidean Length = %.2f',mm);
+        case 3
+            [mm,mI] = max(all_both(:));
+            [mih1,mih2] = ind2sub(size(all_maxc),mI);
+            h1 = hue1_range(mih1);
+            h2 = hue2_range(mih2);
+            casediscr = sprintf('Max weighted sum = %.2f',mm);
+    end
+
+
+h_per_L = (h2-h1)/(Lmax-Lmin);
+
+h = h1 + h_per_L * ((L-Lmin) + L_off - (L-Lmid).^2 * L_off/(Lmax-Lmid)^2);
 h = mod(h,360);
 
 switch typ
     case 'sin'
-        c = sin(pi* (L-L0)/(Lmax-L0) ).^expnt;
+        c = c0 + (1-c0) * sin(pi* (L-Lmin)/(Lmax-Lmin) ).^expnt;
     case 'pow'
-        c = 1 - abs(((L-Lmid)*(min(Lmid-0,100-Lmid)/min(Lmax-Lmid,Lmid-L0))).^expnt) / abs(Lmid.^expnt);
+        c = 1 - (1-c0) * abs(((L-Lmid)*(min(Lmid-0,100-Lmid)/min(Lmax-Lmid,Lmid-Lmin))).^expnt) / abs(Lmid.^expnt);
     otherwise
         error('Unfamiliar type');
 end
@@ -109,122 +191,67 @@ img = repmat(rgb,[1 1 20]);
 img = permute(img,[1 3 2]);
 figure;
 imagesc(img);
+title(sprintf('%.2f %.2f: %s',h1,h2,casediscr));
 
 plot_labcurve_rgbgamut(Lab, use_uplab);
+title(sprintf('%.2f %.2f: %s',h1,h2,casediscr));
+
+end
 
 %%
-% Redo with maxc3
-c = maxc * c;
 
-% Go from cylindrical Lch co-ords to cartesian Lab
-a = c.*cos(h/360*(2*pi));
-b = c.*sin(h/360*(2*pi));
+% h1 = handpicked_hue1;
+% h2 = handpicked_hue2;
 
-% Turn Lab into sRGB values
-Lab = [L' a' b'];
-rgb = soft_lab2rgb(Lab, use_uplab);
+return;
+%%
+[m1,I1] = max(all_both,[],2);
+
+for ih1=1:3:length(hue1_range)
+    ih2 = I1(ih1);
+    h1 = hue1_range(ih1);
+    h2 = hue2_range(ih2);
+    casediscr = '';
+
+h_per_L = (h2-h1)/(Lmax-Lmin);
+
+h = h1 + h_per_L * ((L-Lmin) + L_off - (L-Lmid).^2 * L_off/(Lmax-Lmid)^2);
+h = mod(h,360);
+
+switch typ
+    case 'sin'
+        c = c0 + (1-c0) * sin(pi* (L-Lmin)/(Lmax-Lmin) ).^expnt;
+    case 'pow'
+        c = 1 - (1-c0) * abs(((L-Lmid)*(min(Lmid-0,100-Lmid)/min(Lmax-Lmid,Lmid-Lmin))).^expnt) / abs(Lmid.^expnt);
+    otherwise
+        error('Unfamiliar type');
+end
+
+Lch = [L' c' h'];
 
 % Check for points out of gamut
-[TF,P2] = isingamut(Lab,g,'Lab');
-fprintf('Method #%d now has %d of %d in gamut.\n',method,sum(TF),length(TF));
+[TF,P2] = isingamut(Lch,g,'Lch');
 
-P2C = sqrt(P2(:,2).^2+P2(:,3).^2);
+P2C = P2(:,2);
+maxc = min(P2C./c');
+
+c = c * maxc;
 
 
-% First and last colors may be out due to floating point error
-% others should not be
-% Lab(~TF,:) = P2(~TF,:);
+Lch = [L' c' h'];
+a = c.*cosd(h);
+b = c.*sind(h);
+Lab = [L' a' b'];
 
-% Plot map and its derivation
+rgb = hard_lab2rgb(Lab, use_uplab);
+
 img = repmat(rgb,[1 1 20]);
 img = permute(img,[1 3 2]);
-
 figure;
 imagesc(img);
-axis xy;
-title(sprintf('expnt = %.3f; h1 = %.3f; h/L = %.3f; L.off = %.3f',...
-    expnt, h1, h_per_L, L_off));
+title(sprintf('%.2f %.2f: %s',h1,h2,casediscr));
 
-figure;
-hold on;
-plot(c1, L, 'Color', [0 .8 0]);
-plot(c , L, 'k');
-plot(sqrt(P2(:,2).^2+P2(:,3).^2), P2(:,1), 'r');
-title(sprintf('expnt = %.3f; h1 = %.3f; h/L = %.3f; L.off = %.3f',...
-    expnt, h1, h_per_L, L_off));
-xlabel('chroma');
-ylabel('Lightness');
+% plot_labcurve_rgbgamut(Lab, use_uplab);
+% title(sprintf('%.2f %.2f: %s',h1,h2,casediscr));
 
-figure;
-hold on;
-plot(L,c,'k');
-plot(L,h,'r');
-title(sprintf('expnt = %.3f; h1 = %.3f; h/L = %.3f; L.off = %.3f',...
-    expnt, h1, h_per_L, L_off));
-xlabel('Lightness');
-legend('chroma','hue','Location','EastOutside')
-
-figure;
-scatter3(Lab(:,2), Lab(:,3), Lab(:,1), 20, rgb, 'filled');
-set(gca,'Color',[0.4663 0.4663 0.4663]);
-xlabel('a');
-ylabel('b');
-zlabel('L');
-
-figure;
-hold on;
-plot3(Lab(:,2), Lab(:,3), Lab(:,1), 'bx')
-plot3(P2(1:end-1,2) , P2(1:end-1,3) , P2(1:end-1,1) , 'r-') % Last point is grey so P has hue 0
-xlabel('a');
-ylabel('b');
-zlabel('L');
-view(0,90);
-
-
-res = 17;
-figure('Position',[150 150 550 550]);
-hold on;
-plot3(Lab(:,2), Lab(:,3), Lab(:,1), 'k-');
-% Lab_s = g.lab(g.lab(:,3)>=0,:);
-% rgb_s = g.rgb(g.lab(:,3)>=0,:);
-Lab_s = g.lab;
-rgb_s = g.rgb;
-scatter3(...
-    Lab_s([1:res:end-1 end],2), ...
-    Lab_s([1:res:end-1 end],3), ...
-    Lab_s([1:res:end-1 end],1), ...
-    20, ...
-    rgb_s([1:res:end-1 end],:), ...
-    'filled');
-set(gca,'Color',[0.4663 0.4663 0.4663]);
-set(gca,'XLim',[-150 150],'YLim',[-150 150],'ZLim',[0 100]);
-xlabel('a*')
-ylabel('b*')
-zlabel('L*')
-
-% % Get a mesh version of the gamut
-% if ~isfield(g,'lchmesh')
-%     g.lchmesh = make_gamut_mesh(g);
-% end
-% Lm = g.lchmesh.Lgrid(g.lchmesh.hgrid(:,1)<=180,:);
-% cm = g.lchmesh.cgrid(g.lchmesh.hgrid(:,1)<=180,:);
-% hm = g.lchmesh.hgrid(g.lchmesh.hgrid(:,1)<=180,:)/180*pi;
-% am = cm.*cos(hm);
-% bm = cm.*sin(hm);
-% 
-% cform = makecform('lab2srgb');
-% rgbm = applycform([Lm(:) am(:) bm(:)], cform);
-% chart = 1:size(rgbm,1);
-% chart = reshape(chart,size(Lm));
-% 
-% figure;
-% mesh(am,bm,Lm,chart,'facealpha',0.5,'edgealpha',0.5);
-% colormap(rgbm);
-% hold on;
-% plot3(Lab(:,2), Lab(:,3), Lab(:,1), 'k-');
-% set(gca,'Color',[0.4663 0.4663 0.4663]);
-% xlabel('a');
-% ylabel('b');
-% zlabel('L');
-
-plot_labcurve_rgbgamut(Lab);
+end
