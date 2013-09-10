1d3a5f9aa88cc6c9550e2d62755fc517e53a2ee1
Scott Lowe <scottlowe0@gmail.com>
2013-09-10 17:08:46 +0100

    Fixed cmap so it stays in bounds and goes through grey when not 180 degree sep. No longer variable L option.

--- cie_blueyellow_cmap.m
+++ cie_blueyellow_cmap.m
@@ -6,7 +6,7 @@ if nargin<3 || isempty(dbg)
     dbg = 0; % Whether to output information and figures
 end
 if nargin<2 || isempty(attributes)
-    attributes = 'a0'; % Colormap type option
+    attributes = ''; % Colormap type option
 end
 if nargin<1 || isempty(n)
     n = size(get(gcf,'colormap'),1); % Number of colours in the colormap
@@ -16,6 +16,25 @@ end
 use_uplab = false;
 
 switch attributes
+    case ''
+        % CIE  [  L*     C     h ]
+%         lch1 = [59.375 63.906  98];
+%         lch2 = [59.375 63.906 278];
+        
+%         lch1 = [64.375 68.8125 102.75];
+%         lch2 = [64.375 68.8125 307   ];
+        
+%         lch1 = [59.125 64.375 103];
+%         lch2 = [59.125 64.375 278];
+        
+        lch1 = [59.5   64.5   102];
+        lch2 = [59.5   64.5   282];
+
+    case {'a0','a0:lfix','a0:lfix:1'}
+        % CIE  [  L*     C     h ]
+        lch1 = [63.375 56.968  90];
+        lch2 = [63.375 56.968 270];
+        
     case 'a0'
         % CIE  [  L*    a*  b*]
         lab1 = [  58    0  -65];
@@ -24,6 +43,7 @@ switch attributes
     case {'a0:lfix','a0:lfix:1'}
         % NB: The local maxima for joint-chroma is at h=88 (L=59.5, C=63)
         % So this is pretty much optimal for fixed L
+        % It used to look like a local maxima, but it sure doesn't anymore
         % CIE  [  L*    a*  b*]
         lab1 = [  59.5  0  -62];
         lab2 = [  59.5  0   62];
@@ -40,23 +60,52 @@ switch attributes
 %         lab2 = [ 90.00 -52   84];
 
     case 'primaries'
-%         lch1 = [29.5628  131.2104  301.3643];
-%         lch2 = [97.6067   94.7101   99.5585];
-        lab1 = [ 29.5628   68.2921 -112.0373];
-        lab2 = [ 97.6067  -15.7270   93.3952];
+        
+%         lab1 = [97.1279  -21.5484   94.4728]; % yellow
+%         lab2 = [32.3006   79.1839 -107.8569]; % blue
+        
+        lch1 = [97.1279   96.8991  102.8489]; % yellow
+        lch2 = [32.3006  133.8029  306.2845]; % blue
         
     otherwise
         error('Unfamiliar colormap attribute: %s',attributes);
 end
 
 % -------------------------------------------------------------------------
-L = linspace(lab1(1), lab2(1), n);
-a = linspace(lab1(2), lab2(2), n);
-b = linspace(lab1(3), lab2(3), n);
+lab1 = [lch1(1) lch1(2).*cosd(lch1(3)) lch1(2).*sind(lch1(3))];
+lab2 = [lch2(1) lch2(2).*cosd(lch2(3)) lch2(2).*sind(lch2(3))];
 
-Lab = [L' a' b'];
+if mod(lch1(3)-lch2(3),360)==180
 
-cmap = soft_lab2rgb(Lab, use_uplab);
+    L = linspace(lab1(1), lab2(1), n);
+    a = linspace(lab1(2), lab2(2), n);
+    b = linspace(lab1(3), lab2(3), n);
+
+    Lab = [L' a' b'];
+
+else
+    L_grey = (lch1(1)*lch2(2)+lch2(1)*lch1(2))/(lch1(2)+lch2(2));
+
+    neach = floor(n/2)+1;
+    % => if n=odd  then neach+(neach-1)=n
+    %    if n=even then neach+(neach-1)=n+1
+
+    L1 = linspace(lab1(1), L_grey, neach);
+    a1 = linspace(lab1(2),      0, neach);
+    b1 = linspace(lab1(3),      0, neach);
+
+    L2 = linspace(L_grey, lab2(1), neach);
+    a2 = linspace(     0, lab2(2), neach);
+    b2 = linspace(     0, lab2(3), neach);
+
+    Lab1 = [L1' a1' b1'];
+    Lab2 = [L2' a2' b2'];
+    
+    % Convert from Lab to srgb
+    Lab  = [Lab1(1:end-1,:); Lab2];
+end
+
+cmap = hard_lab2rgb(Lab, use_uplab);
 
 % -------------------------------------------------------------------------
 % If dbg mode, display a figure of the outputted colormap
@@ -68,7 +117,7 @@ if dbg;
     axis xy;
     title('Output colormap');
     
-    plot_labcurve_rgbgamut(Lab)
+    plot_labcurve_rgbgamut(Lab, use_uplab);
 end
 
 end
