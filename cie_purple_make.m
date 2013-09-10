6c6ce0fd1fdf716a47406525630a23c4c0106155
Scott Lowe <scottlowe0@gmail.com>
2013-09-10 17:00:02 +0100

    Added a CIELab based purple cmap, (maximal chroma hot style)

---
+++ cie_purple_make.m
@@ -0,0 +1,39 @@
+function cmap = cie_purple_make(n, attr, dbg)
+
+% -------------------------------------------------------------------------
+% Default inputs
+if nargin<3 || isempty(dbg)
+    dbg = 0; % Whether to output information and figures
+end
+if nargin<2
+    attr = []; % Unused
+end
+if nargin<1 || isempty(n)
+    n = size(get(gcf,'colormap'),1); % Number of colours in the colormap
+end
+
+% -------------------------------------------------------------------------
+% Get params
+params = get_params();
+params.n = n;
+
+% -------------------------------------------------------------------------
+cmap = cmap_pinchedspiral_make(params, dbg);
+
+end
+
+%==========================================================================
+function params = get_params()
+
+params.use_uplab = false;
+params.typ       = 'pow'; % 'pow' or 'sin'
+params.expnt     = 2;     % exponent to use for chroma curve
+params.L_off     = 0;     % offset to exponent (not for sine curve)
+params.c0        = 0.025; % proportion of maxc to add to all chroma (maxc\approx75, 67<maxc<77)
+params.Lmin      = 4;     % minimum lightness (start)
+params.Lmax      = 92;    % maximum lightness (end)
+params.h1        = 308.5;
+params.h2        = 331;
+params.maxc      = 105.9;
+
+end
