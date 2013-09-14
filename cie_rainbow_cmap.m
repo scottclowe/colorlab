e7e772b53caf70aa1d558d6e01af648121a88fa5
Scott Lowe <scottlowe0@gmail.com>
2013-09-14 14:17:25 +0100

    Added for backward compatibility until rainbow map is fixed

---
+++ cie_rainbow_cmap.m
@@ -0,0 +1,21 @@
+function cmap = cie_rainbow_cmap(n, attr, dbg)
+
+% -------------------------------------------------------------------------
+% Default inputs
+if nargin<3 || isempty(dbg)
+    dbg = 0; % Whether to output information and figures
+end
+if nargin<2 || isempty(attr)
+    attr = 'greenmid'; % Colormap type option
+end
+if nargin<1 || isempty(n)
+    n = size(get(gcf,'colormap'),1); % Number of colours in the colormap
+end
+
+% -------------------------------------------------------------------------
+makefun = [mfilename '_make'];
+makefun = str2func(makefun);
+
+cmap = load_cmap(n, makefun, attr, dbg);
+
+end
