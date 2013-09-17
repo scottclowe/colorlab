cc95ae53199284261386e375ff8f52cade242a35
Scott Lowe <scottlowe0@gmail.com>
2013-09-17 22:53:12 +0100

    Fixed so c is always dynamic. Now L can be dynamic too (default) and h0 is rotated 15deg

--- clab_wheel.m
+++ clab_wheel.m
@@ -7,40 +7,72 @@
 %   
 %   Scott Lowe, April 2013
 
-function [rgb,params] = clab_wheel(n, L, c, h0, h1, dbg)
-
-% -------------------------------------------------------------------------
-if nargin<6
-    dbg = true;
-end
-if nargin<4
-    h0 = 0;
-end
-if nargin<5
-    h1 = h0+2*pi;
-end
-if nargin<3
-    c = 60; % chroma
-end
-if nargin<2
-    L = 60; % Lightness
-    L = 73.875;
-end
-if nargin>1 && nargin<4
-    % Find from srgb gamut the largest chroma which is available for all H
-    rgbgamut = fetch_cielchab_gamut('srgb');
-    c = min(rgbgamut.lchmesh.cgrid(:,rgbgamut.lchmesh.Lvec==L));
-end
-
-% Default with same number of colors as in use for current colormap
-if nargin<1 || isempty(n)
-    n = size(get(gcf,'colormap'),1);
-end
+function [rgb,params] = clab_wheel(n, L, h0, h1, dbg)
 
 % -------------------------------------------------------------------------
 use_uplab = false;
 
 % -------------------------------------------------------------------------
+% Default with same number of colors as in use for current colormap
+if nargin<1 || isempty(n)
+    n = size(get(gcf,'colormap'),1);
+end
+if nargin<2 || isempty(L)
+%     L = 50; % Lightness
+%     L = 60; % Lightness
+%     L = 73.875;
+    L = [];
+end
+if nargin<3 || isempty(h0)
+    h0 = 15;
+    if use_uplab
+        h0 = h0+15;
+    end
+end
+if nargin<4 || isempty(h1)
+    h1 = h0+360;
+end
+if nargin<5
+    dbg = true;
+end
+
+% -------------------------------------------------------------------------
+rgbgamut = fetch_cielchab_gamut('srgb');
+
+% Find out which h values are included
+if h1<h0
+    h_srt = h1;
+    h_end = h0;
+else
+    h_srt = h0;
+    h_end = h1;
+end
+if h_end-h_srt >=360
+    h_srt = 0;
+    h_end = 360;
+elseif h_end > 360
+    h_srt = h_srt - 360;
+    h_end = h_end - 360;
+end
+
+% Find the largest chroma which is available for all H
+if h_srt>=0
+    hli = rgbgamut.lchmesh.hvec >= h_srt & rgbgamut.lchmesh.hvec <= h_end;
+else
+    hli = rgbgamut.lchmesh.hvec <= h_end | rgbgamut.lchmesh.hvec >= (h_srt+360);
+end
+
+if isempty(L)
+    cc = min(rgbgamut.lchmesh.cgrid(hli,:),[],1);
+    [c,Lli] = max(cc);
+    L = rgbgamut.lchmesh.Lvec(Lli);
+else
+    Lli = rgbgamut.lchmesh.Lvec==L;
+    c = min(rgbgamut.lchmesh.cgrid(hli,Lli));
+end
+
+% -------------------------------------------------------------------------
+
 h = linspace(h0, h1, (n+1)).';
 h = h(1:end-1);
 
@@ -71,5 +103,6 @@ params.L  = L;
 params.c  = c;
 params.h0 = h0;
 params.h1 = h1;
+params.use_uplab = use_uplab;
 
 end
