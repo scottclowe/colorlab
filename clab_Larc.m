88971e7cdcf49d9bebdeaa7132659815d853128d
Scott Lowe <scottlowe0@gmail.com>
2013-09-18 23:29:59 +0100

    Swapped to a fixed h0 and h1 for all L

--- clab_Larc.m
+++ clab_Larc.m
@@ -21,19 +21,48 @@ use_cmax = false;
 rgbgamut = fetch_cielchab_gamut('srgb',[],[],use_uplab);
 
 if ~use_uplab
+%     % Use obsevered L values only (could interpolate between these)
 %     edgL  = [ 15     20    25    30    35    40    45    50    55    60    65     70      75  ];
 %     edgh0 = [-90.75 -85.5 -83.5 -82.5 -82.5 -82.5 -82.5 -82.5 -82.5 -83.5 -66.0 -108.75 -145  ];
 %     edgh1 = [158.75 153.0 150.5 149.5 149.5 149.5 149.5 149.5 149.5 150.5 156.75 178     215  ];
 %     edgc  = [ 21.9   28.3  33.9  38.8  43.1  47.3  51.5  55.8  60.0  62.8  59.2  46.4     38.4];
     
-    [h0,h1,c] = arcparams_cielab(L, rgbgamut);
+%     % Use automated definitions based on observed search spaces
+%     [h0,h1,c] = arcparams_cielab(L, rgbgamut);
+    
+    % Use same h0 and h1 for all L
+    h0 = -82.5;
+    h1 = 149.5;
+    if h0>=0
+        hli = rgbgamut.lchmesh.hvec >= h0 & rgbgamut.lchmesh.hvec <= h1;
+    else
+        hli = rgbgamut.lchmesh.hvec <= h1 | rgbgamut.lchmesh.hvec >= (h0+360);
+    end
+    % Look up chroma
+    c = min(rgbgamut.lchmesh.cgrid(hli, rgbgamut.lchmesh.Lvec==L));
     
 else
-    % Stuff here
+%     % Use obsevered L values only (could interpolate between these)
 %     edgL  = [ 40     45    50     55     60     65    70     75   ];
 %     edgh0 = [-54    -54   -53    -46.75 -55.75 -63   -77.5  -140  ];
 %     edgh1 = [ 57.75  57.5  56.75  57     67.50  86.5 193.75  220  ];
 %     edgc  = [ 72.5   80    87     90     75     62.9  50      40.1];
+    
+%     % Use automated definitions based on observed search spaces
+%     [h0,h1,c] = arcparams_uplab(L, rgbgamut);
+    
+    % Use same h0 and h1 for all L
+    h0 = -72; % include all above second-min chroma: h0 = -72;
+%     h0 = -49; % start at the top of the blue ridge:  h0 = -49;
+    h1 = 190;
+    if h0>=0
+        hli = rgbgamut.lchmesh.hvec >= h0 & rgbgamut.lchmesh.hvec <= h1;
+    else
+        hli = rgbgamut.lchmesh.hvec <= h1 | rgbgamut.lchmesh.hvec >= (h0+360);
+    end
+    % Look up chroma
+    c = min(rgbgamut.lchmesh.cgrid(hli, rgbgamut.lchmesh.Lvec==L));
+    
 end
 
 % -------------------------------------------------------------------------
@@ -60,13 +89,12 @@ rgb = hard_lab2rgb(Lab, use_uplab);
 
 % -------------------------------------------------------------------------
 % If dbg mode, display a figure of the outputted colormap
-if dbg;
-    img = repmat(rgb,[1 1 20]);
-    img = permute(img,[1 3 2]);
+if dbg
     figure;
-    imagesc(img);
+    imagesc(permute(rgb,[1 3 2]));
     axis xy;
     title('Output colormap');
+    
     plot_labcurve_rgbgamut(Lab, use_uplab);
 end
 
@@ -84,7 +112,7 @@ end
 function [h0,h1,c] = arcparams_cielab(L, rgbgamut)
     % ---------------------------------------------------------------------
     if nargin<2
-        rgbgamut = fetch_cielchab_gamut('srgb',[],[],use_uplab);
+        rgbgamut = fetch_cielchab_gamut('srgb',[],[],false);
     end
     % ---------------------------------------------------------------------
     %  0<L<61:  Min1 at h=214, Min2 at h=93
@@ -118,6 +146,38 @@ function [h0,h1,c] = arcparams_cielab(L, rgbgamut)
 end
 
 % =========================================================================
+function [h0,h1,c] = arcparams_uplab(L, rgbgamut)
+    % ---------------------------------------------------------------------
+    if nargin<2
+        rgbgamut = fetch_cielchab_gamut('srgb',[],[],true);
+    end
+    % ---------------------------------------------------------------------
+    
+    % Define our seach space for second minima
+    % This was done very quickly and is not optimal
+    if L<70
+        h_srt = 0;
+        h_end = 166;
+    else
+        h0 = -145;
+        h1 =  215;
+        c  = min(rgbgamut.lchmesh.cgrid(:, rgbgamut.lchmesh.Lvec==L));
+        return;
+    end
+    
+    if h_srt>=0
+        hli = rgbgamut.lchmesh.hvec >= h_srt & rgbgamut.lchmesh.hvec <= h_end;
+    else
+        hli = rgbgamut.lchmesh.hvec <= h_end | rgbgamut.lchmesh.hvec >= (h_srt+360);
+    end
+    % Look up chroma
+    cc = rgbgamut.lchmesh.cgrid(:, rgbgamut.lchmesh.Lvec==L);
+    c  = 0.99*min(cc(hli,:));
+    h0 = rgbgamut.lchmesh.hvec(find(~(cc>=c),1,'last' )+1)-360;
+    h1 = rgbgamut.lchmesh.hvec(find(~(cc>=c),1,'first')-1);
+end
+
+% =========================================================================
 % Search code
 
 % cc = rgbgamut.lchmesh.cgrid(:,rgbgamut.lchmesh.Lvec==30);
@@ -129,6 +189,7 @@ end
 % figure;
 % imagesc(rgbgamut.lchmesh.Lvec, rgbgamut.lchmesh.hvec, bsxfun(@rdivide, rgbgamut.lchmesh.cgrid, max(rgbgamut.lchmesh.cgrid)))
 % colormap(clab_hot); colorbar; xlabel('L'); ylabel('h');
+% 
 % figure;
 % imagesc(rgbgamut.lchmesh.Lvec, rgbgamut.lchmesh.hvec, bsxfun(@rdivide, 1./rgbgamut.lchmesh.cgrid, max(1./rgbgamut.lchmesh.cgrid)))
 % colormap(clab_hot); colorbar; xlabel('L'); ylabel('h');
