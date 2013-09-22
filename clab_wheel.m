9b172eb7f1e5e22abd08121215b569e1b965e3b9
Scott Lowe <scottlowe0@gmail.com>
2013-09-22 01:21:33 +0100

    Multi L support (interspersed or seperate outputs)

--- clab_wheel.m
+++ clab_wheel.m
@@ -1,13 +1,13 @@
 %LABWHEEL A set of colors from a circle in CIELAB color space
-%   RGB = LABWHEEL(N,L,C,H0) finds a set of N colors equally spaced in
+%   RGB = LABWHEEL(N,LL,C,H0) finds a set of N colors equally spaced in
 %   CIELAB space in a circle using CIELCHab co-ordinates. The circle is
-%   taken anti-clockwise with constant lightness, L and contant chroma, C,
+%   taken anti-clockwise with constant lightness, LL and contant chroma, C,
 %   and an initial Hue of H0. Returns RGB, an n-by-3 vector of these colors
 %   represented in sRGB space.
 %   
 %   Scott Lowe, April 2013
 
-function [rgb,params] = clab_wheel(n, L, h0, h1, dbg)
+function [varargout] = clab_wheel(n, LL, h0, h1, dbg)
 
 % -------------------------------------------------------------------------
 use_uplab = false;
@@ -17,11 +17,8 @@ use_uplab = false;
 if nargin<1 || isempty(n)
     n = size(get(gcf,'colormap'),1);
 end
-if nargin<2 || isempty(L)
-%     L = 50; % Lightness
-%     L = 60; % Lightness
-%     L = 73.875;
-    L = [];
+if nargin<2 || isempty(LL)
+    LL = [];
 end
 if nargin<3 || isempty(h0)
     h0 = 15; % Default start hue with CIELab
@@ -38,8 +35,17 @@ end
 
 % -------------------------------------------------------------------------
 % Input parsing
-if ischar(h0); h0 = colorstr2h(h0); end
-if ischar(h1); h1 = colorstr2h(h1); end
+clrstr = false;
+if ischar(h0); h0=colorstr2h(h0); clrstr=true; end
+if ischar(h1); h1=colorstr2h(h1); clrstr=true; end
+
+if clrstr && abs(h0-h1)>180
+    if h1>h0;
+        h1 = h1-360;
+    else
+        h0 = h0-360;
+    end
+end
 
 % -------------------------------------------------------------------------
 rgbgamut = fetch_cielchab_gamut('srgb');
@@ -67,13 +73,15 @@ else
     hli = rgbgamut.lchmesh.hvec <= h_end | rgbgamut.lchmesh.hvec >= (h_srt+360);
 end
 
-if isempty(L)
+if isempty(LL)
+    % If no L is given, find the optimal L giving maximum chroma
     cc = min(rgbgamut.lchmesh.cgrid(hli,:),[],1);
-    [c,Lli] = max(cc);
-    L = rgbgamut.lchmesh.Lvec(Lli);
+    [cc,Lli] = max(cc);
+    LL = rgbgamut.lchmesh.Lvec(Lli);
 else
-    Lli = rgbgamut.lchmesh.Lvec==L;
-    c = min(rgbgamut.lchmesh.cgrid(hli,Lli));
+    % If L is given, find optimal chroma for each L given
+    Lli = ismember(rgbgamut.lchmesh.Lvec,LL);
+    cc = min(rgbgamut.lchmesh.cgrid(hli,Lli));
 end
 
 % -------------------------------------------------------------------------
@@ -85,33 +93,57 @@ else
     h = linspace(h0, h1, n).';
 end
 
-a = c*cosd(h);
-b = c*sind(h);
-LL = repmat(L,n,1);
-
-Lab = [LL a b];
-
-rgb = hard_lab2rgb(Lab, use_uplab);
+aa = bsxfun(@times, cc, cosd(h));
+bb = bsxfun(@times, cc, sind(h));
 
 % -------------------------------------------------------------------------
-% If dbg mode, display a figure of the outputted colormap
-if dbg;
-    img = repmat(rgb,[1 1 20]);
-    img = permute(img,[1 3 2]);
-    figure;
-    imagesc(img);
-    axis xy;
-    title('Output colormap');
-    plot_labcurve_rgbgamut(Lab, use_uplab);
+% Now pick out these colours
+
+LL_Lab = cell(length(LL),1);
+LL_rgb = cell(length(LL),1);
+
+for j=1:length(LL)
+    L = repmat(LL(j),n,1);
+    a = aa(:,j);
+    b = bb(:,j);
+    Lab = [L a b];
+    rgb = hard_lab2rgb(Lab, use_uplab);
+    LL_Lab{j} = Lab;
+    LL_rgb{j} = rgb;
 end
 
 % -------------------------------------------------------------------------
-if nargout<2; return; end
-params.n  = n;
-params.L  = L;
-params.c  = c;
-params.h0 = h0;
-params.h1 = h1;
-params.use_uplab = use_uplab;
+% Output all in one matrix, or in individual matrices for each L
+% depending on number of outputs
+if nargout>1
+    varargout = LL_rgb;
+else
+    rgb = cell2mat(LL_rgb);
+    I = bsxfun(@plus,(1:n),n*(0:(length(LL)-1))');
+    varargout = {rgb(I(:),:)};
+end
+
+% -------------------------------------------------------------------------
+% If dbg mode, display a figure of the outputted colormap
+if dbg
+    rgb = cell2mat(varargout);
+    figure;
+    imagesc(permute(rgb,[1 3 2]));
+    axis xy;
+    title('Output colormap');
+    
+    LL_Lab2 = nan(n*length(LL),3);
+    I = setdiff(1:((n+1)*length(LL)),(n+1)*(1:length(LL)));
+    LL_Lab2(I,:) = cell2mat(LL_Lab);
+    plot_labcurve_rgbgamut(LL_Lab2, use_uplab);
+end
+% -------------------------------------------------------------------------
+% if nargout<2; return; end
+% params.n  = n;
+% params.LL  = LL;
+% params.c  = c;
+% params.h0 = h0;
+% params.h1 = h1;
+% params.use_uplab = use_uplab;
 
 end
