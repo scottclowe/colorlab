660f7ef4e40e52aa9af75231d6eb232e858a181c
Scott Lowe <scott.code.lowe@gmail.com>
2017-10-11 18:56:24 +0100

    ENH: Allow lch2lab to take inputs with any number of dimensions

--- spaceconversion/lch2lab.m
+++ spaceconversion/lch2lab.m
@@ -1,13 +1,19 @@
 function lab = lch2lab(lch)
 
-if size(lch,2)~=3
-    error('Input must be n-by-3 matrix');
+siz = size(lch);
+
+if siz(end) ~= 3
+    error('Input must have size 3 in final dimension.');
 end
+
+lch = reshape(lch, [], 3);
 
 L = lch(:,1);
 a = lch(:,2) .* cosd(lch(:,3));
 b = lch(:,2) .* sind(lch(:,3));
 
 lab = [L a b];
+
+lab = reshape(lab, siz);
 
 end
